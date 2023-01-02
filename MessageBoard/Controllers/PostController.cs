8fe1401ee6b52c03178d73eee4ab60fcda425f66
availablenick <leoap711@gmail.com>
2023-01-02 19:26:36 -0300

    refactor: remove unused imports

--- MessageBoard/Controllers/PostController.cs
+++ MessageBoard/Controllers/PostController.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Security.Claims;
 
 using MessageBoard.Auth;
 using MessageBoard.Data;
--- MessageBoard/Controllers/UserController.cs
+++ MessageBoard/Controllers/UserController.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using System.ComponentModel.DataAnnotations;
-using System.Security.Claims;
 
 using MessageBoard.Auth;
 using MessageBoard.Data;
