f22d92f9557bdd7f4d92b08d40a80e2be5a6df7b
availablenick <leoap711@gmail.com>
2023-01-15 18:02:36 -0300

    refactor: remove post validation from Model

--- MessageBoard/Models/Post.cs
+++ MessageBoard/Models/Post.cs
@@ -3,9 +3,4 @@ namespace MessageBoard.Models;
 public class Post : Rateable
 {
     public Discussion? Discussion { get; set; }
-
-    public bool IsValid()
-    {
-        return !String.IsNullOrEmpty(Content);
-    }
 }
